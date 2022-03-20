cc82adf9ff6c385cf2b3941e5c5990da46eb778a
zhanna.aharon@gmail.com <zhanna.aharon@gmail.com>
2022-03-20 15:55:15 +0200

    Created a project

---
+++ MS1Solution/ConApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConApp1 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
